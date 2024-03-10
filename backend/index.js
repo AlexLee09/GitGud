7c6cf79ced255564085df1ffa953e4d03f488086
AlexLee09 <alexandrekmlee@gmail.com>
2024-03-10 22:48:13 +0800

    Render data from backend and main page styles.

--- backend/index.js
+++ backend/index.js
@@ -68,6 +68,28 @@ app.post('/user/request', async(req, res) =>
 
         console.log(input)
 
+        const dataToSend = 
+        {
+            data: [
+                {
+                    command: `git checkout -b "[Branch Name]"`,
+                    explanation: "Git Checkout is a command in the Git version control system that allows users to switch between different branches or versions of a project."
+                },
+                {
+                    command: "git add .",
+                    explanation: "The git add command adds a change in the working directory to the staging area. It tells Git that you want to include updates to a particular file in the next commit. However, git add doesn't really affect the repository in any significant way—changes are not actually recorded until you run git commit."
+                },
+                {
+                    command: `git commit -m "[Commit message here]"`,
+                    explanation: `The git commit command is what you'll use to take all of the changes that have been made locally and push them up to a remote repository. It's important to note that you can't just type "git commit" by itself with no arguments — it needs at least one parameter, which will be either HEAD or any other branch name.`
+                },
+                {
+                    command: "git push",
+                    explanation: "The git push command is used to upload local repository content to a remote repository. Pushing is how you transfer commits from your local repository to a remote repo."
+                }
+            ]
+        }
+
         res.status(200).json(dataToSend); 
     }
     catch (error) {
--- frontend/src/pages/MainPage.js
+++ frontend/src/pages/MainPage.js
@@ -1,19 +1,21 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import classes from './MainPage.module.css'; 
 
 function MainPage()
 {
     const navigate = useNavigate();
 
-   
     const returnToLanding = () =>
     {
         navigate("/");
     }
 
     const[input, setInput] = useState("");
+
+    const[data, setData] = useState([]);
     
-    const handleSubmit = async (event) =>
+    const handleSubmit = async (event) => 
     {
         event.preventDefault();
 
@@ -30,19 +32,32 @@ function MainPage()
 
         const data = await response.json(); 
 
-        console.log (data);
+        setData(data.data); 
     }
 
+    return (
+        <div className = {classes.container}>
+            <div className = {classes.tile1}>
+                <h1>Main Page</h1>
+                <textarea placeholder='Type question here' onChange={(event) => setInput(event.target.value)} className={classes.questionInput}>
+                </textarea>
+                <button onClick = {returnToLanding}>Return</button>
+                <button onClick={handleSubmit}>SUBMIT</button>
+            </div>
+            <div className = {classes.tile2}>
+                <ol>
+                    { data.length > 0 && data.map ((item, index) => { 
+                        return (
+                            <li key={index}>
+                                { item.command }
+                            </li>
+                        )
+                    })}
+                </ol>
 
-    
-    return(
-        <div>
-            <h1>Main Page</h1>
-            <button onClick = {returnToLanding}>Return</button>
-            <textarea placeholder='Type question here'
-            onChange={(event) => setInput(event.target.value)}>    
-            </textarea>
-            <button onClick={handleSubmit}>SUBMIT</button>
+                <textarea readOnly>Mentaiko carbonara combines the richness of traditional carbonara with the umami punch of mentaiko (marinated cod roe). Creamy, savory, and slightly spicy, it's a delightful fusion that tantalizes taste buds. </textarea>
+
+            </div>
         </div>
     )
 }
